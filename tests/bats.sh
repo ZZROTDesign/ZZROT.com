54bda84ac28962128de6932ea3743748a4d045a4
Killian Brackey <killianbrackey@gmail.com>
2016-04-14 05:26:05 -0500

    Test cleanup

--- tests/bats.sh
+++ tests/bats.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bats
 
 @test "It should respond with HTTP 200 Okay" {
-	run curl --write-out "%{http_code}\n" --silent --output /dev/null 172.17.0.1
+	run curl --write-out "%{http_code}\n" --silent --output /dev/null localhost
 	[[ "$output" =~ "200" ]]
 }
 
 @test "blog should return 200" {
-  run curl --write-out "%{http_code}\n" --silent --output /dev/null 172.17.0.1:2368
+  run curl --write-out "%{http_code}\n" --silent --output /dev/null localhost:2368
 	#[[ ${lines[0]} =~ "<!doctype html>" ]]
   [[ "$output" =~ "200" ]]
 }

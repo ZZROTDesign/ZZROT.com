4edc46c1c2d239a896df68c998365e13b8fe527f
Killian Brackey <killianbrackey@gmail.com>
2016-04-17 00:06:42 -0500

    Edited tests for Jenkins

--- testDriver.sh
+++ testDriver.sh
@@ -1,11 +1,12 @@
 # Driver that runs after normal build tests and then
 # opens the links, logging the page status for
 # review, but not a test currently
+bats tests/bats.sh
 
 echo ''
 # First CLI is link, second is maximum page count to open
 echo "Spidering links from home page"
-casperjs tests/spider.js http://192.168.99.100 10
+casperjs tests/spider.js localhost 10
 echo ''
 echo "Spidering links from blog"
-casperjs tests/spider.js http://192.168.99.100:2368 20
+casperjs tests/spider.js localhost 20

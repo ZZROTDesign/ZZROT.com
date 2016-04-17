44bec8ad4c921e3f393cba3f9e80d1e9344b3078
Killian Brackey <killianbrackey@gmail.com>
2016-04-17 02:33:26 -0500

    Added page status txt file for spidering

--- testDriver.sh
+++ testDriver.sh
@@ -13,12 +13,10 @@ blogUrl=http://162.243.15.248:2368
 # Repeat of the bats tests
 casperjs test tests/tests.js --homeUrl=$homeUrl --blogUrl=$blogUrl
 
-#echo
-# First CLI is link, second is maximum page count to open
-#echo "Spidering links from home page"
-
-#set -x
-#casperjs tests/spider.js $homeUrl 10 >> spider.txt
-#echo ''
-#echo "Spidering links from blog"
-#casperjs tests/spider.js $blogUrl 20 >> spider.txt
+#First CLI is link, second is maximum page count to open
+rm tests/pageStatus.txt
+echo "Spidering links from home page" >> tests/pageStatus.txt
+casperjs tests/spider.js $homeUrl 10 >> tests/pageStatus.txt
+echo ''
+echo "Spidering links from blog"
+casperjs tests/spider.js $blogUrl 20 >> tests/pageStatus.txt

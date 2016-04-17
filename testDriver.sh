f94f9a58c81de3c9b299e5e24ac4695ba27d6d55
Killian Brackey <killianbrackey@gmail.com>
2016-04-17 03:13:25 -0500

    Edited tests for Jenkins build

--- testDriver.sh
+++ testDriver.sh
@@ -14,11 +14,11 @@ blogUrl=http://162.243.15.248:2368
 casperjs test tests/tests.js --homeUrl=$homeUrl --blogUrl=$blogUrl
 
 # Don't fail build because of these status outputs
-set +e
+#set +e
 #First CLI is link, second is maximum page count to open
-rm tests/pageStatus.txt
-echo "Spidering links from home page" >> tests/pageStatus.txt
-casperjs tests/spider.js $homeUrl 10 >> tests/pageStatus.txt
-echo ''
-echo "Spidering links from blog"
-casperjs tests/spider.js $blogUrl 20 >> tests/pageStatus.txt
+#rm tests/pageStatus.txt
+#echo "Spidering links from home page" >> tests/pageStatus.txt
+#casperjs tests/spider.js $homeUrl 10 >> tests/pageStatus.txt
+#echo ''
+#echo "Spidering links from blog"
+#casperjs tests/spider.js $blogUrl 20 >> tests/pageStatus.txt
